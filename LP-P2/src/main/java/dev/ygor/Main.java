2e7f9ffe1592433e60c85246a710eeef7cc24e3a
YgorPereira <ygorrp25@gmail.com>
2024-11-25 22:15:23 -0300

    atualizando o dao

--- LP-P2/src/main/java/dev/ygor/Main.java
+++ LP-P2/src/main/java/dev/ygor/Main.java
@@ -6,39 +6,40 @@ import dev.ygor.model.Aluno;
 
 import javax.swing.*;
 import java.sql.Connection;
+import java.sql.SQLException;
 
 
 public class Main {
     public static void main(String[] args) {
         ConnectionFactory connectionFactory = new ConnectionFactory();
 
-        try (Connection connection = connectionFactory.getConnections()) {
-            // Instanciar AlunoDAO com a conexão criada
+        try(Connection connection = connectionFactory.getConnections()) {
             AlunoDAO alunoDAO = new AlunoDAO(connection);
 
-            // Criar um novo aluno
-            Aluno novoAluno = new Aluno("Maria Oliveira", "98765432100", "1995-08-15", 68.0, 1.65);
-            novoAluno.calculaIMC();
-            novoAluno.interpretaIMC();
+            Aluno Luana = new Aluno("Luana Souza", "98765432100", "2006-11-17", 55.0, 1.55);
+            Aluno Ygor = new Aluno("Ygor Pereira", "12345678900", "1995-08-15", 68.0, 1.65);
 
-            // Cadastrar o aluno no banco de dados
-            alunoDAO.cadastrarAluno(novoAluno);
-            alunoDAO.inserirImcESituacao(novoAluno);
-            System.out.println("Aluno cadastrado com sucesso!");
+            alunoDAO.cadastrarAluno(Luana);
+            alunoDAO.cadastrarAluno(Ygor);
 
-            // Buscar aluno pelo nome
-            Aluno alunoEncontrado = alunoDAO.buscarAlunoPorNome("Maria Oliveira");
-            if (alunoEncontrado != null) {
-                System.out.println("Aluno encontrado:");
-                System.out.println("Nome: " + alunoEncontrado.getNome());
-                System.out.println("IMC: " + alunoEncontrado.getImc());
-                System.out.println("Situação IMC: " + alunoEncontrado.getSituacaoIMC());
-            } else {
-                System.out.println("Aluno não encontrado.");
-            }
+            Ygor.calculaIMC();
+            Ygor.interpretaIMC();
+            alunoDAO.inserirImcESituacao(Ygor);
 
-//            exlui o aluno da tabela
-//            alunoDAO.excluirAlunoPorCPF(novoAluno.getCpf());
+            Luana.calculaIMC();
+            Luana.interpretaIMC();
+            alunoDAO.inserirImcESituacao(Luana);
+
+            Ygor.gravarNumArquivo();
+            Luana.gravarNumArquivo();
+
+            alunoDAO.buscarAlunoPorNome(Ygor.getNome());
+
+            alunoDAO.excluirAlunoPorCPF("98765432100");
+
+            Ygor.setPeso(60);
+
+            alunoDAO.update(Ygor);
         } catch (Exception e) {
             System.err.println("Erro durante a execução: " + e.getMessage());
         }
